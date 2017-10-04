403793c3ed1fe1075728bbde2b3cfaa1d4c93411
parkerduckworth <parkerduckworth@gmail.com>
2017-10-03 21:59:57 -0700

    add controller actions

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -1,2 +1,37 @@
 class CreatorsController < ApplicationController
+	before_action :find_creator, only: [:edit, :update, :show, :destroy]
+
+	def index
+		@creators = Creator.all
+	end
+
+	def new
+		@creator = Creator.new
+	end
+
+	def create
+		@creator.save
+		redirect_to creators_path
+	end
+
+	def edit
+	end
+
+	def update
+		if @creator.update(creator_params)
+			flash[:success] = "Creator was successfully updated"
+			redirect_to creator_path(@creator)
+		else
+			render 'edit'
+		end
+	end
+
+	private
+		def find_creator
+			@creator = Creator.find(params[:id])
+		end
+
+		def creator_params
+			params.require(:creator).permit(:name, :blurb, :email, :password, :creator_img)
+		end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user.save
+		redirect_to creators_path
+	end
 end
